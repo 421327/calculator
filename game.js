b8a1d025e795e350967099502e2fd35d0321cf59
421327 <109552536+421327@users.noreply.github.com>
2023-10-13 23:39:54 +0300

    Update game.js

--- game.js
+++ game.js
@@ -2,11 +2,11 @@ var game = new Phaser.Game(100,60,  Phaser.AUTO);
 
 var GameState = {
     preload: function () {
-        //this.load.image('bg', 'assets/images/test.png')
+        this.load.image('bg', 'test.png')
     },
     
     create: function () {
-       //this.bg = this.game.add.sprite(0,0,'bg')
+       this.bg = this.game.add.sprite(0,0,'bg')
     },
     
     update: function () {
